bd34b4f7d86531b099ae937f43c6f02a67e84387
Taher Najim <taher.najim@yahoo.com>
2024-05-23 19:32:40 +0800

    fixed duplicate results

--- phasebook/search.py
+++ phasebook/search.py
@@ -20,25 +20,37 @@ def search_users(args):
     # id search
     if 'id' in args:
         matchingId = [obj for obj in USERS if obj['id'] == args['id']]
-        result_list.append(matchingId[0])
+        # check if matchingId exists then check if its id is already in added_ids
+        if matchingId and matchingId[0]['id'] not in added_ids:
+            result_list.append(matchingId[0])
+            added_ids.add(matchingId[0]['id'])
 
     # name search
     if 'name' in args:
         search_name = args['name'].lower()
         # iterates USERS then checks if 'name' is in obj['name'] if true adds obj to result_list
         matchingNames = [obj for obj in USERS if search_name in obj['name'].lower()]
-        result_list.extend(matchingNames)
+        for obj in matchingNames:
+            if obj['id'] not in added_ids:
+                result_list.append(obj)
+                added_ids.add(obj['id'])
 
     # age search
     if 'age' in args:
         search_age = int(args['age'])
         matchingAges = [obj for obj in USERS if search_age - 1 <= obj['age'] and obj['age'] <= search_age + 1]
-        result_list.extend(matchingAges)
+        for obj in matchingAges:
+            if obj['id'] not in added_ids:
+                result_list.append(obj)
+                added_ids.add(obj['id'])
 
-    # occupation = partially matched and is case insensitive
+    # occupation search
     if 'occupation' in args:
         search_occupations = args['occupation'].lower()
         matchingOccupations = [obj for obj in USERS if search_occupations in obj['occupation'].lower()]
-        result_list.extend(matchingOccupations)
+        for obj in matchingOccupations:
+            if obj['id'] not in added_ids:
+                result_list.append(obj)
+                added_ids.add(obj['id'])
 
     return result_list
